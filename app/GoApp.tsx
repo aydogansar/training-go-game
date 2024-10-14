89e6f23dd96b0c5258ce9bdca54f7485f594fc38
Aydoğan Sarı <aydogansari@hotmail.com>
2024-10-14 22:05:05 +0300

    Update page

---
+++ app/GoApp.tsx
@@ -0,0 +1,76 @@
+'use client';
+
+import { useRef, useState } from 'react';
+
+import { Button } from '@/components/ui/button';
+import { toast } from '@/hooks/use-toast';
+import { GameRefProps } from '@/modules/goban/types';
+import Goban from '@/modules/goban';
+import { GAME } from '@/modules/goban/constants';
+import { ToggleGroup, ToggleGroupItem } from '@/components/ui/toggle-group';
+
+function GoApp() {
+  const ref = useRef<GameRefProps>(null);
+
+  const [size, setSize] = useState(9);
+
+  return (
+    <div className="flex justify-center items-center h-[100vh] gap-5 flex-col md:flex-row">
+      <Goban
+        ref={ref}
+        size={size}
+        initialWidth={1200}
+        showCoordinates
+        onError={error => {
+          toast({
+            variant: 'destructive',
+            description: error.message,
+          });
+        }}
+      />
+
+      <div className="flex flex-col gap-2">
+        <ToggleGroup
+          type="single"
+          value={size.toString()}
+          onValueChange={value => setSize(+value)}
+        >
+          <ToggleGroupItem
+            variant="outline"
+            value="9"
+          >
+            9x9
+          </ToggleGroupItem>
+          <ToggleGroupItem
+            variant="outline"
+            value="13"
+            disabled
+          >
+            13x13
+          </ToggleGroupItem>
+          <ToggleGroupItem
+            variant="outline"
+            value="19"
+            disabled
+          >
+            19x19
+          </ToggleGroupItem>
+        </ToggleGroup>
+
+        <Button
+          variant="destructive"
+          onClick={() => ref.current?.undoLastMove()}
+        >
+          Geri Al
+        </Button>
+        <Button
+          variant="secondary"
+          onClick={() => ref.current?.passTurn()}
+        >
+          Pas
+        </Button>
+      </div>
+    </div>
+  );
+}
+export default GoApp;
--- app/page.tsx
+++ app/page.tsx
@@ -1,45 +1,9 @@
-'use client';
-
-import { Button } from '@/components/ui/button';
-import { toast } from '@/hooks/use-toast';
-import { GameRefProps } from '@/modules/goban/types';
-import Goban from '@/modules/goban';
-import { GAME } from '@/modules/goban/constants';
-import { useRef } from 'react';
+import GoApp from './GoApp';
 
 export default function Home() {
-  const ref = useRef<GameRefProps>(null);
-
   return (
-    <div className="flex justify-center items-center h-[100vh] gap-5 flex-col md:flex-row">
-      <Goban
-        ref={ref}
-        initialPlayer={GAME.WHITE}
-        size={9}
-        initialWidth={600}
-        showCoordinates
-        onError={error => {
-          toast({
-            variant: 'destructive',
-            description: error.message,
-          });
-        }}
-      />
-
-      <div className="flex flex-col gap-2">
-        <Button
-          variant="destructive"
-          onClick={() => ref.current?.undoLastMove()}
-        >
-          Geri Al
-        </Button>
-        <Button
-          variant="secondary"
-          onClick={() => ref.current?.passTurn()}
-        >
-          Pas
-        </Button>
-      </div>
+    <div>
+      <GoApp />
     </div>
   );
 }
