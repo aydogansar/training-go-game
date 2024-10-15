28bb3fb6ce6a7a523e9093ed3a60890afeefa199
Aydoğan Sarı <aydogansari@hotmail.com>
2024-10-15 19:43:49 +0300

    Update GoApp import with dynamic import with no ssr

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,21 @@
-import GoApp from './GoApp';
+import { Skeleton } from '@/components/ui/skeleton';
+import dynamic from 'next/dynamic';
+
+const GoDemo = dynamic(() => import('./GoApp'), {
+  ssr: false,
+  loading: () => (
+    <div className="flex items-center justify-center min-h-screen p-4">
+      <div className="w-full max-w-[800px] aspect-square">
+        <Skeleton className="h-full" />
+      </div>
+    </div>
+  ),
+});
 
 export default function Home() {
   return (
     <div>
-      <GoApp />
+      <GoDemo />
     </div>
   );
 }
