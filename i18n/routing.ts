c5bad8aceea4c8678a3288fb59a124cac5c0660d
Aydoğan Sarı <aydogansari@hotmail.com>
2024-10-18 20:57:07 +0300

    Update next-intl configs

--- i18n/routing.ts
+++ i18n/routing.ts
@@ -1,5 +1,6 @@
 import { defineRouting } from 'next-intl/routing';
 import { createSharedPathnamesNavigation } from 'next-intl/navigation';
+import { ComponentProps } from 'react';
 
 export const routing = defineRouting({
   // A list of all locales that are supported
@@ -7,8 +8,16 @@ export const routing = defineRouting({
 
   // Used when no locale matches
   defaultLocale: 'tr',
+  pathnames: {
+    '/': '/',
+    '/problems': {
+      tr: '/alistirmalar-2',
+    },
+  },
 });
 
 // Lightweight wrappers around Next.js' navigation APIs
 // that will consider the routing configuration
 export const { Link, redirect, usePathname, useRouter } = createSharedPathnamesNavigation(routing);
+
+export type LinkProps = ComponentProps<typeof Link>;
